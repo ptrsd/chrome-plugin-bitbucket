32a0ece75995c439cdbd273abefbe438993cbdd7
ptrsd <piotr@siudy.info>
2017-08-18 17:54:38 +0200

    making possible to toggle visibility of deleted files

--- myscript.js
+++ myscript.js
@@ -1,21 +1,19 @@
 var hideDeletesAction = function() {
 	$(".diff-entry-lozenge.aui-lozenge.aui-lozenge-subtle.aui-lozenge-error").each(function(idx, elem) {
-		var diffContainer = $(elem).closest("div.diff-container");
-		
-		$(diffContainer.find(".diff-content-container.refract-container")[0]).css({
-			'display': 'none'
-		})
-
+		var diffContainer = $(elem).closest("div.diff-container");		
+        $(diffContainer.find(".diff-content-container.refract-container")[0]).toggle();
 		diffContainer.find("div.heading").css({
-			'borderBottom': '1px solid #ccc',
-			'borderRadius': '5px 5px 5px 5px'
-		});
+            'borderBottom': '1px solid #ccc',
+            'borderRadius': '5px 5px 5px 5px'
 	});
+
+	$("#hideDeletes").toggleClass("aui-button-primary");
 }
 
 var hideDeletesLbl = $("<span>").html("Hide deletes");
 
 var hideDeletesBtn = $("<button>", {
+	id: "hideDeletes",
 	class: "aui-button",
 	resolved: ""
 }).html(hideDeletesLbl).click(hideDeletesAction)
@@ -25,4 +23,3 @@ $("#pull-request-header")
 	.find(".clearfix")
 	.find("#pullrequest-actions")
 	.append(hideDeletesBtn);
-
