62d2c2659d53a17dac21541f7380ecdc7af0034e
ptrsd <piotr@siudy.info>
2017-08-18 18:10:56 +0200

    setting code style configuration

--- myscript.js
+++ myscript.js
@@ -1,25 +1,26 @@
-var hideDeletesAction = function() {
-	$(".diff-entry-lozenge.aui-lozenge.aui-lozenge-subtle.aui-lozenge-error").each(function(idx, elem) {
-		var diffContainer = $(elem).closest("div.diff-container");		
-        $(diffContainer.find(".diff-content-container.refract-container")[0]).toggle();
-		diffContainer.find("div.heading").css({
-            'borderBottom': '1px solid #ccc',
-            'borderRadius': '5px 5px 5px 5px'
-	});
+var hideDeletesAction = function () {
+  $(".diff-entry-lozenge.aui-lozenge.aui-lozenge-subtle.aui-lozenge-error").each(function (idx, elem) {
+    var diffContainer = $(elem).closest("div.diff-container");
+    $(diffContainer.find(".diff-content-container.refract-container")[0]).toggle();
+    diffContainer.find("div.heading").css({
+      'borderBottom': '1px solid #ccc',
+      'borderRadius': '5px 5px 5px 5px'
+    });
 
-	$("#hideDeletes").toggleClass("aui-button-primary");
+    $("#hideDeletes").toggleClass("aui-button-primary");
+  });
 }
 
 var hideDeletesLbl = $("<span>").html("Hide deletes");
 
 var hideDeletesBtn = $("<button>", {
-	id: "hideDeletes",
-	class: "aui-button",
-	resolved: ""
-}).html(hideDeletesLbl).click(hideDeletesAction)
+  id: "hideDeletes",
+  class: "aui-button",
+  resolved: ""
+}).html(hideDeletesLbl).click(hideDeletesAction);
 
 
 $("#pull-request-header")
-	.find(".clearfix")
-	.find("#pullrequest-actions")
-	.append(hideDeletesBtn);
+  .find(".clearfix")
+  .find("#pullrequest-actions")
+  .append(hideDeletesBtn);
